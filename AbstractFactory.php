ebbd29eeba4d41446ff7d0916d54c15d665eb7cd
MeatyBanan4ik <bbananov69@gmail.com>
2021-09-12 21:56:14 +0300

    Update

--- AbstractFactory.php
+++ AbstractFactory.php
@@ -1,2 +1,78 @@
 <?php
-interface product
+interface Product
+{
+    public function getName();
+}
+
+abstract class AbstractFactory
+{
+    public static function choseProduct($name)
+    {
+        switch (strtolower($name))
+        {
+            case 'food':
+                return new FoodFactory();
+            case 'clothes':
+                return new ClothesFactory();
+            default:
+                exit('Что то пошло не по плану');
+        }
+    }
+
+    abstract public function getProduct();
+}
+
+class FoodFactory extends AbstractFactory
+{
+    public function getProduct()
+    {
+        return new Food();
+    }
+}
+
+class Food implements Product
+{
+    public function getName()
+    {
+        return "Какая то еда";
+    }
+}
+
+class ClothesFactory extends AbstractFactory
+{
+    public function getProduct()
+    {
+        return new Clothes();
+    }
+}
+
+class Clothes implements Product
+{
+    public function getName()
+    {
+        return "Какая то одежда";
+    }
+}
+
+$food = AbstractFactory::choseProduct('food')->getProduct();
+echo $food->getName();
+
+echo '<br>';
+echo '<br>';
+echo '<pre>';
+
+print_r($food);
+
+echo '</pre>';
+echo '<br>';
+echo '<br>';
+
+$clothes = AbstractFactory::choseProduct('clothes')->getProduct();
+echo $clothes->getName();
+
+echo '<br>';
+echo '<br>';
+echo '<pre>';
+print_r($clothes);
+echo '</pre>';
+
